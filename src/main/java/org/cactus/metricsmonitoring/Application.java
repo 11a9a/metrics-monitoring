8d38ef8aa7b281c650c2dee419161f6662a8a9f6
11a9a <m.mammadli11@gmail.com>
2023-10-10 01:34:16 +0400

    Jaeger and Zipkin implemented
    Implemented Jaeger and Zipkin to have some playground to play with OpenTelemetry Connector, Jaeger and Zipkin.

--- src/main/java/org/cactus/metricsmonitoring/Application.java
+++ src/main/java/org/cactus/metricsmonitoring/Application.java
@@ -2,12 +2,38 @@ package org.cactus.metricsmonitoring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder){
+        return restTemplateBuilder.build();
+    }
+
+    @RestController
+    class HelloController{
+        private final RestTemplate restTemplate;
+
+        HelloController(RestTemplate restTemplate) {
+            this.restTemplate = restTemplate;
+        }
+
+        @GetMapping("/hello")
+        public String hello(){
+            ResponseEntity<String> responseEntity = this.restTemplate.postForEntity("https://httpbin.org/post", "Hello!!!", String.class);
+            return responseEntity.getBody();
+        }
     }
 
 }
