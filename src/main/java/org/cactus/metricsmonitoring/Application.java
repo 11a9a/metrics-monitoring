18bdcdd5bfe6f998d7abb75ae663f1735d45cdb2
11a9a <m.mammadli11@gmail.com>
2023-10-19 02:33:12 +0400

    Loki implemented
    Implemented Loki as logging datasource, while keeping Prometheus, Tempo as metrics and tracing datasources.

--- src/main/java/org/cactus/metricsmonitoring/Application.java
+++ src/main/java/org/cactus/metricsmonitoring/Application.java
@@ -1,10 +1,13 @@
 package org.cactus.metricsmonitoring;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -17,12 +20,13 @@ public class Application {
     }
 
     @Bean
-    public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder){
+    public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
         return restTemplateBuilder.build();
     }
 
     @RestController
-    class HelloController{
+    static class HelloController {
+        private static final Logger LOGGER = LoggerFactory.getLogger(HelloController.class);
         private final RestTemplate restTemplate;
 
         HelloController(RestTemplate restTemplate) {
@@ -30,10 +34,22 @@ public class Application {
         }
 
         @GetMapping("/hello")
-        public String hello(){
+        public String hello() {
+            LOGGER.info("---------Hello method started!               ---------");
+            LOGGER.error("---------Hello method started, id is missing!---------");
             ResponseEntity<String> responseEntity = this.restTemplate.postForEntity("https://httpbin.org/post", "Hello!!!", String.class);
             return responseEntity.getBody();
         }
-    }
 
+        @GetMapping("/exception")
+        public String exception() {
+            throw new IllegalArgumentException("This id is invalid");
+        }
+
+        @ExceptionHandler(value = {IllegalArgumentException.class})
+        protected ResponseEntity<String> handleConflict(IllegalArgumentException ex) {
+            LOGGER.error(ex.getMessage(), ex);
+            return ResponseEntity.badRequest().body(ex.getMessage());
+        }
+    }
 }
